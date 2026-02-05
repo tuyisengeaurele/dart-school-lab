a8878a6c927ee74830fb635ea906177c97fd7a63
tuyisengeaurele <157024701+tuyisengeaurele@users.noreply.github.com>
2026-02-05 21:40:47 +0200

    main combining all

--- main.dart
+++ main.dart
@@ -1,5 +1,21 @@
+import 'annonymous_arrow_functions.dart';
+import 'asynchronous.dart';
+import 'classes_constructors.dart';
+import 'collections.dart';
+import 'functions.dart';
+import 'inheritance.dart';
 import 'updated_mixin.dart';
 
-void main() {
+void main() async {
+  welcomeMessage();
+  createStudent(name: "Aurele", age: 22);
+  createTeacher("Ruth");
+  createTeacher("Ruth", "Mobile Application Systems and Design");
+  testStudent();
+  testInheritance();
   testUpdatedMixin();
+  testCollections();
+  testAnonymousFunction(["Ruth", "Aurele", "Gad"]);
+  greetStudent("Ruth");
+  testAsync();
 }
